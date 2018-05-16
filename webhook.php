dc9955d19783e9af7ccbbb963827cd6c442bdc22
kano <chk392013@gmail.com>
2018-05-16 19:04:17 +0900

    updated webhook

--- webhook.php
+++ webhook.php
@@ -15,3 +15,5 @@ if ( isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='
     // 認証失敗
     file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
 }
+
+?>
