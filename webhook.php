6b7b197fb5f3c1e44f34c3b2f2d38def8171dfdd
kano <chk392013@gmail.com>
2018-05-16 19:32:26 +0900

    updated webhook

--- webhook.php
+++ webhook.php
@@ -1,19 +1,18 @@
 <?php
 
-// 設定
-$LOG_FILE = dirname(__FILE__).'/hook.log';
-$SECRET_KEY = 'security2018';　//githubで設定するsecretもところ
 
+// 設定 
+$secret_key ="security2018";
 $header = getallheaders();
-$hmac = hash_hmac('sha1', $HTTP_RAW_POST_DATA, $SECRET_KEY);
+$hmac = hash_hmac('sha1', $HTTP_RAW_POST_DATA, $secret_key);
 if ( isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.$hmac ) {
-    $payload = json_decode($HTTP_RAW_POST_DATA, true);  // 受け取ったJSONデータ
+    //$payload = json_decode($HTTP_RAW_POST_DATA, true);  // 受け取ったJSONデータ
     // ここに実行したいコードを書く(testレポに移動して、git pullする)
     exec('git pull');
-    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['after']." ".$payload['commits'][0]['message']."\n", FILE_APPEND|LOCK_EX);
+    echo "OK";
 } else {
-    // 認証失敗
-    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
+  echo "error";
 }
+
 
 ?>
